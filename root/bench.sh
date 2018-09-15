103097f77f75f7560355ab9be653296012c74537
owlworks <owlworks@users.noreply.github.com>
2018-09-15 13:57:28 +0900

    Edit kataribe.toml for URL reg

--- root/bench.sh
+++ root/bench.sh
@@ -7,7 +7,7 @@ cp /var/log/mariadb/mariadb-slow.log /var/log/mariadb/mariadb-slow.log.old
 rm /var/log/mariadb/mariadb-slow.log
 systemctl restart mariadb
 
-cat /var/log/nginx/kataribe.log | kataribe | slackcat
+cat /var/log/nginx/kataribe.log | kataribe [-f /root/kataribe.toml] | slackcat --filename "kataribe-$(hostname).txt"
 cp /var/log/nginx/kataribe.log /var/log/nginx/kataribe.log.old
 rm -f /var/log/nginx/kataribe.log
 systemctl restart nginx
