3fbfec45480414047e816c571efe9465c4a7b03c
bgpat <bgpat@users.noreply.github.com>
2018-09-15 11:53:08 +0900

    Extract user.go

--- home/isucon/torb/webapp/go/src/torb/app.go
+++ home/isucon/torb/webapp/go/src/torb/app.go
@@ -20,13 +20,6 @@ import (
 	"github.com/labstack/echo/middleware"
 )
 
-type User struct {
-	ID        int64  `json:"id,omitempty"`
-	Nickname  string `json:"nickname,omitempty"`
-	LoginName string `json:"login_name,omitempty"`
-	PassHash  string `json:"pass_hash,omitempty"`
-}
-
 type Event struct {
 	ID       int64  `json:"id,omitempty"`
 	Title    string `json:"title,omitempty"`
@@ -72,46 +65,6 @@ type Reservation struct {
 	Price          int64  `json:"price,omitempty"`
 	ReservedAtUnix int64  `json:"reserved_at,omitempty"`
 	CanceledAtUnix int64  `json:"canceled_at,omitempty"`
-}
-
-func sessUserID(c echo.Context) int64 {
-	sess, _ := session.Get("session", c)
-	var userID int64
-	if x, ok := sess.Values["user_id"]; ok {
-		userID, _ = x.(int64)
-	}
-	return userID
-}
-
-func sessSetUserID(c echo.Context, id int64) {
-	sess, _ := session.Get("session", c)
-	sess.Options = &sessions.Options{
-		Path:     "/",
-		MaxAge:   3600,
-		HttpOnly: true,
-	}
-	sess.Values["user_id"] = id
-	sess.Save(c.Request(), c.Response())
-}
-
-func sessDeleteUserID(c echo.Context) {
-	sess, _ := session.Get("session", c)
-	sess.Options = &sessions.Options{
-		Path:     "/",
-		MaxAge:   3600,
-		HttpOnly: true,
-	}
-	delete(sess.Values, "user_id")
-	sess.Save(c.Request(), c.Response())
-}
-
-func loginRequired(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		if _, err := getLoginUser(c); err != nil {
-			return resError(c, "login_required", 401)
-		}
-		return next(c)
-	}
 }
 
 func getEvents(all bool) ([]*Event, error) {
---
+++ home/isucon/torb/webapp/go/src/torb/user.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo-contrib/session"
+)
+
+type User struct {
+	ID        int64  `json:"id,omitempty"`
+	Nickname  string `json:"nickname,omitempty"`
+	LoginName string `json:"login_name,omitempty"`
+	PassHash  string `json:"pass_hash,omitempty"`
+}
+
+func sessUserID(c echo.Context) int64 {
+	sess, _ := session.Get("session", c)
+	var userID int64
+	if x, ok := sess.Values["user_id"]; ok {
+		userID, _ = x.(int64)
+	}
+	return userID
+}
+
+func sessSetUserID(c echo.Context, id int64) {
+	sess, _ := session.Get("session", c)
+	sess.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   3600,
+		HttpOnly: true,
+	}
+	sess.Values["user_id"] = id
+	sess.Save(c.Request(), c.Response())
+}
+
+func sessDeleteUserID(c echo.Context) {
+	sess, _ := session.Get("session", c)
+	sess.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   3600,
+		HttpOnly: true,
+	}
+	delete(sess.Values, "user_id")
+	sess.Save(c.Request(), c.Response())
+}
+
+func loginRequired(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		if _, err := getLoginUser(c); err != nil {
+			return resError(c, "login_required", 401)
+		}
+		return next(c)
+	}
+}
