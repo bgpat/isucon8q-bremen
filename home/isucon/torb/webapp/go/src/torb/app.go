da756a75d0ed81c279730fca5e2cb0a0458cc36d
bgpat <bgpat@users.noreply.github.com>
2018-09-15 12:08:45 +0900

    Move reservation

--- home/isucon/torb/webapp/go/src/torb/app.go
+++ home/isucon/torb/webapp/go/src/torb/app.go
@@ -39,22 +39,6 @@ type Sheet struct {
 	ReservedAtUnix int64      `json:"reserved_at,omitempty"`
 }
 
-type Reservation struct {
-	ID         int64      `json:"id"`
-	EventID    int64      `json:"-"`
-	SheetID    int64      `json:"-"`
-	UserID     int64      `json:"-"`
-	ReservedAt *time.Time `json:"-"`
-	CanceledAt *time.Time `json:"-"`
-
-	Event          *Event `json:"event,omitempty"`
-	SheetRank      string `json:"sheet_rank,omitempty"`
-	SheetNum       int64  `json:"sheet_num,omitempty"`
-	Price          int64  `json:"price,omitempty"`
-	ReservedAtUnix int64  `json:"reserved_at,omitempty"`
-	CanceledAtUnix int64  `json:"canceled_at,omitempty"`
-}
-
 func sanitizeEvent(e *Event) *Event {
 	sanitized := *e
 	sanitized.Price = 0
--- home/isucon/torb/webapp/go/src/torb/reserve.go
+++ home/isucon/torb/webapp/go/src/torb/reserve.go
@@ -9,6 +9,22 @@ import (
 	"github.com/labstack/echo"
 )
 
+type Reservation struct {
+	ID         int64      `json:"id"`
+	EventID    int64      `json:"-"`
+	SheetID    int64      `json:"-"`
+	UserID     int64      `json:"-"`
+	ReservedAt *time.Time `json:"-"`
+	CanceledAt *time.Time `json:"-"`
+
+	Event          *Event `json:"event,omitempty"`
+	SheetRank      string `json:"sheet_rank,omitempty"`
+	SheetNum       int64  `json:"sheet_num,omitempty"`
+	Price          int64  `json:"price,omitempty"`
+	ReservedAtUnix int64  `json:"reserved_at,omitempty"`
+	CanceledAtUnix int64  `json:"canceled_at,omitempty"`
+}
+
 func postReserve(c echo.Context) error {
 	eventID, err := strconv.ParseInt(c.Param("id"), 10, 64)
 	if err != nil {
