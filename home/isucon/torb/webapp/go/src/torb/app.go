bdb047a5134007178a39c289785baf2ab005c90c
bgpat <bgpat@users.noreply.github.com>
2018-09-15 11:33:50 +0900

    Extract admin

--- home/isucon/torb/webapp/go/src/torb/app.go
+++ home/isucon/torb/webapp/go/src/torb/app.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"database/sql"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"html/template"
 	"io"
@@ -77,13 +76,6 @@ type Reservation struct {
 	CanceledAtUnix int64  `json:"canceled_at,omitempty"`
 }
 
-type Administrator struct {
-	ID        int64  `json:"id,omitempty"`
-	Nickname  string `json:"nickname,omitempty"`
-	LoginName string `json:"login_name,omitempty"`
-	PassHash  string `json:"pass_hash,omitempty"`
-}
-
 func sessUserID(c echo.Context) int64 {
 	sess, _ := session.Get("session", c)
 	var userID int64
@@ -115,37 +107,6 @@ func sessDeleteUserID(c echo.Context) {
 	sess.Save(c.Request(), c.Response())
 }
 
-func sessAdministratorID(c echo.Context) int64 {
-	sess, _ := session.Get("session", c)
-	var administratorID int64
-	if x, ok := sess.Values["administrator_id"]; ok {
-		administratorID, _ = x.(int64)
-	}
-	return administratorID
-}
-
-func sessSetAdministratorID(c echo.Context, id int64) {
-	sess, _ := session.Get("session", c)
-	sess.Options = &sessions.Options{
-		Path:     "/",
-		MaxAge:   3600,
-		HttpOnly: true,
-	}
-	sess.Values["administrator_id"] = id
-	sess.Save(c.Request(), c.Response())
-}
-
-func sessDeleteAdministratorID(c echo.Context) {
-	sess, _ := session.Get("session", c)
-	sess.Options = &sessions.Options{
-		Path:     "/",
-		MaxAge:   3600,
-		HttpOnly: true,
-	}
-	delete(sess.Values, "administrator_id")
-	sess.Save(c.Request(), c.Response())
-}
-
 func loginRequired(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		if _, err := getLoginUser(c); err != nil {
@@ -153,35 +114,6 @@ func loginRequired(next echo.HandlerFunc) echo.HandlerFunc {
 		}
 		return next(c)
 	}
-}
-
-func adminLoginRequired(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		if _, err := getLoginAdministrator(c); err != nil {
-			return resError(c, "admin_login_required", 401)
-		}
-		return next(c)
-	}
-}
-
-func getLoginUser(c echo.Context) (*User, error) {
-	userID := sessUserID(c)
-	if userID == 0 {
-		return nil, errors.New("not logged in")
-	}
-	var user User
-	err := db.QueryRow("SELECT id, nickname FROM users WHERE id = ?", userID).Scan(&user.ID, &user.Nickname)
-	return &user, err
-}
-
-func getLoginAdministrator(c echo.Context) (*Administrator, error) {
-	administratorID := sessAdministratorID(c)
-	if administratorID == 0 {
-		return nil, errors.New("not logged in")
-	}
-	var administrator Administrator
-	err := db.QueryRow("SELECT id, nickname FROM administrators WHERE id = ?", administratorID).Scan(&administrator.ID, &administrator.Nickname)
-	return &administrator, err
 }
 
 func getEvents(all bool) ([]*Event, error) {
@@ -279,15 +211,6 @@ func fillinUser(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		if user, err := getLoginUser(c); err == nil {
 			c.Set("user", user)
-		}
-		return next(c)
-	}
-}
-
-func fillinAdministrator(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		if administrator, err := getLoginAdministrator(c); err == nil {
-			c.Set("administrator", administrator)
 		}
 		return next(c)
 	}
